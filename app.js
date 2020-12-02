bcd93e149d8f5466d9bbc9fdd28163754bbe7b0e
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2020-12-02 18:12:20 +0530

    feat: translation added

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 
 // alert("Connected");
-var input = document.querySelector('#input-txt');
-var btnClick = document.querySelector('#btn');
-var outpt = document.querySelector('.output');
+var input = document.querySelector("#input-txt");
+var btnClick = document.querySelector("#btn");
+var otpt = document.querySelector(".output");
+var server = "https://api.funtranslations.com/translate/minion.json";
 
-function clickHandler()
-{
-    console.log("Clicked");
+
+function getTranslateURL(txt){
+    return server + "?" + "text=" + txt;
 }
+
+function clickHandler(){
+    var inputTxt = input.value;
+    fetch(getTranslateURL(inputTxt))
+        .then(response => response.json())
+        .then(json =>{
+        var translatedTxt = json.contents.translated;
+    otpt.innerText = translatedTxt;
+        
+        }
+    
+    )
+};
 
 btnClick.addEventListener("click",clickHandler);
 
