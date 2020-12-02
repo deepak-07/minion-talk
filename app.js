1ba620c9fd862a8834ab7418b1687800a3949fba
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2020-12-02 16:42:23 +0530

    feat: button connected

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// alert("Connected");
+var input = document.querySelector('#input-txt');
+var btnClick = document.querySelector('#btn');
+var outpt = document.querySelector('.output');
+
+function clickHandler()
+{
+    console.log("Clicked");
+}
+
+btnClick.addEventListener("click",clickHandler);
+
+
+
+
+
